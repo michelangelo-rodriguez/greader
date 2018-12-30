088647c703d55be69ac38d3f4668f77adb917c57
Michelangelo Rodriguez <michelangelo.rodriguez@gmail.com>
2018-12-30 15:49:20 +0100

    group greader-espeak added

--- greader-espeak.el
+++ greader-espeak.el
@@ -1,3 +1,8 @@
 ;;; greader-espeak
 ;;; A greader's back-end for espeak synthesizer.
 					;Copyright (C) 2018 by Michelangelo Rodriguez
+(defgroup greader-espeak-backend
+  "back-end of espeak for greader."
+  :group 'greader
+  )
+
