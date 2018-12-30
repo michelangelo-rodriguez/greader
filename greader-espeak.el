733f1d4616278c92f7bbb63039ef375e3277dac5
Michelangelo Rodriguez <michelangelo.rodriguez@gmail.com>
2018-12-30 18:26:40 +0100

    defcustom greader-espeak-language added.

--- greader-espeak.el
+++ greader-espeak.el
@@ -1,2 +1,17 @@
 ;;; greader-espeak
 ;;; A greader's back-end for espeak synthesizer.
+					;Copyright (C) 2018 by Michelangelo Rodriguez
+(defgroup greader-espeak-backend
+nil
+  "back-end of espeak for greader."
+  :group 'greader
+  )
+
+(defcustom
+  greader-espeak-language
+  "en"
+  "specifies the language of this back-end. For a comprehensive list of languages and voices available in espeak type in a terminal:
+espeak --list-languages
+"
+  :tag "greader espeak language"
+  :type 'string)
