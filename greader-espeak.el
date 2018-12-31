804d9a71090d65b3b516b8c89231c0385954339a
Michelangelo Rodriguez <michelangelo.rodriguez@gmail.com>
2018-12-31 07:38:34 +0100

    lexical binding enabled

--- greader-espeak.el
+++ greader-espeak.el
@@ -1,5 +1,5 @@
-;;; greader-espeak
-;;; A greader's back-end for espeak synthesizer.
+;;; greader-espeak --- espeak back-end for greader -*- lexical-binding: t; -*-
+
 					;Copyright (C) 2018 by Michelangelo Rodriguez
 (defgroup greader-espeak-backend
 nil
@@ -15,3 +15,9 @@ espeak --list-languages
 "
   :tag "greader espeak language"
   :type 'string)
+(defcustom
+  greader-espeak-rate
+  200
+  "Specifies the rate os speech in words per minute."
+  :tag "greader espeak rate"
+  :type 'integer)
