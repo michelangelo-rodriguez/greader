076f8b79bd9bf5b2ec2a1dae90669d787b33968e
Michelangelo Rodriguez <michelangelo.rodriguez@gmail.com>
2019-01-02 17:50:05 +0100

    greader-speechd.el: custom variable  greader-speechd-executable added.

--- greader-speechd.el
+++ greader-speechd.el
@@ -2,3 +2,15 @@
 					;Copyright (C) 2019 by Michelangelo Rodriguez
 
 
+;;; customization variables
+(defgroup greader-speechd
+  nil
+  "speech-dispatcher back-end for greader"
+  :group 'greader)
+
+(defcustom
+  greader-speechd-executable
+  "spd-say"
+  "executable file name."
+  :tag "speech-dispatcher client executable file name"
+  :type 'string)
