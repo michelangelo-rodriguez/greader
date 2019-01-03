cf2681c38b4f64c7f6d4ad67dc2312eb49f901b5
Michelangelo Rodriguez <michelangelo.rodriguez@gmail.com>
2019-01-03 06:52:44 +0100

    greader-speechd.el: Added custom variables greader-speechd-executable-path, greader-speechd-language and greader-speechd-rate.

--- greader-speechd.el
+++ greader-speechd.el
@@ -14,3 +14,22 @@
   "executable file name."
   :tag "speech-dispatcher client executable file name"
   :type 'string)
+(defcustom
+  greader-speechd-executable-path
+  (greader-speechd-find-executable)
+  "Path of speech-dispatcher client executable."
+  :tag "speechd client executable path"
+  :type 'string)
+(defcustom
+  greader-speechd-language
+  "en"
+  "specifies language of speech-dispatcher client to speak in."
+  :tag "speech-dispatcher language"
+  :type 'string)
+(defcustom
+  greader-speechd-rate
+  10
+  "specifies rate of speech.
+(From -100 to 100.)"
+  :tag "speech-dispatcher rate"
+  :type 'integer)
